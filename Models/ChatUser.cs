1b85ab3d98501de23eed54dcba1fa35cb9f49632
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-10-03 22:53:45 +0530

    Attached the GeoLocation here

--- Models/ChatUser.cs
+++ Models/ChatUser.cs
@@ -12,6 +12,8 @@ namespace RealTimeChatApplication.Models
         public string? ConfirmPassword { get; set; }
         public IFormFile? ProfilePictureURL { get; set; }
         public bool? Status { get; set; }
+        public string? Longitude { get; set; }
+        public string? Latitude { get; set; }
 
     }
 }
