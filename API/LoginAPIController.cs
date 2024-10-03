8281a00d33ff50df8ac2f0dd4564adfd8c1f06ce
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-10-03 06:52:22 +0530

    Create the User Successfully

--- API/LoginAPIController.cs
+++ API/LoginAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RealTimeChatApplication.Models;
+using System.Data.SqlClient;
 
 namespace RealTimeChatApplication.API
 {
@@ -8,10 +9,33 @@ namespace RealTimeChatApplication.API
     [ApiController]
     public class LoginAPIController : ControllerBase
     {
+        private readonly string _connectionString;
+
+        public LoginAPIController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CustomConnection");
+        }
+
+
+
         [HttpPost]
         public IActionResult Register([FromBody] ChatUser pChatUser)
         {
-            return Ok();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("usp_ChatUser", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Mode", 1);
+                    cmd.Parameters.AddWithValue("@UserName", pChatUser.UserName);
+                    cmd.Parameters.AddWithValue("@Password", pChatUser.Password);
+                    cmd.Parameters.AddWithValue("@Email", pChatUser.Email);
+                    cmd.ExecuteNonQuery();
+                }
+                return Ok();
+            }
         }
     }
 }
--- Models/ChatUser.cs
+++ Models/ChatUser.cs
@@ -5,13 +5,13 @@ namespace RealTimeChatApplication.Models
     public class ChatUser
     {
 
-        public int ChatUserID { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
-        public IFormFile ProfilePictureURL { get; set; }
-        public bool Status { get; set; }
+        public int? ChatUserID { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public IFormFile? ProfilePictureURL { get; set; }
+        public bool? Status { get; set; }
 
     }
 }
