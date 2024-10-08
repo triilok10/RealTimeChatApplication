c21fd270766e1200519a7ec9cba9954d3ed56993
Trilok Chand Swami <ittiku3@gmail.com>
2024-10-08 22:34:11 +0530

    Receiving the Message to the Controller now using the ClientSide SignalR

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -5,9 +5,14 @@ namespace RealTimeChatApplication.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        public async Task SendMessage( string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
         }
     }
 }
