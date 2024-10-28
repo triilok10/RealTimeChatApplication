af1214fe8bec7c0854395e5e0907ff4954d92757
triilok10 <ittiku3@gmail.com>
2024-10-28 21:43:20 +0530

    Update URL and Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ using RealTimeChatApplication.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddSignalR();
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
@@ -18,7 +17,7 @@ builder.Services.AddScoped<RedirectIfLoggedInAttribute>();
 builder.Services.AddScoped<ISessionService, SessionService>();
 builder.Services.AddScoped<IChatHub, ChatHub>();
 
-// Configure session settings
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(5);
@@ -26,35 +25,47 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+
 var firebaseConfig = builder.Configuration.GetSection("Firebase").Get<FirebaseConfig>();
+var serviceAccountPath = Path.Combine(Directory.GetCurrentDirectory(),
+    firebaseConfig.ServiceAccountFile.TrimStart('~', '/').Replace('/', Path.DirectorySeparatorChar));
 
-var serviceAccountPath = Path.Combine(Directory.GetCurrentDirectory(), firebaseConfig.ServiceAccountFile.TrimStart('~', '/').Replace('/', Path.DirectorySeparatorChar));
 
-FirebaseApp.Create(new AppOptions
+if (FirebaseApp.DefaultInstance == null)
 {
-    Credential = GoogleCredential.FromFile(serviceAccountPath)
-});
+    FirebaseApp.Create(new AppOptions
+    {
+        Credential = GoogleCredential.FromFile(serviceAccountPath)
+    });
+}
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+// Common Middleware
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+}
 
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseSession();
-    app.UseAuthorization();
-
-    app.MapHub<ChatHub>("/chathub");
-
-    app.MapControllerRoute(
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chathub");
+    endpoints.MapControllerRoute(
         name: "default",
         pattern: "{controller=Login}/{action=Login}/{id?}");
+});
 
-    app.Run();
-}
+app.Run();
