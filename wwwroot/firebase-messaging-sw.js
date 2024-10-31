9feec04694543db060ddc1f0a6f89796e337f526
triilok10 <ittiku3@gmail.com>
2024-10-31 19:46:50 +0530

    Code modify for Firebase Version 9.22.0 and fcm token generation successfully done.

---
+++ wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+// Import Firebase libraries
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyBIWES3c3n3-JXMAwSfp1bNZQ6oZ3yQGyc",
+    authDomain: "realtimechatapplication-trilok.firebaseapp.com",
+    projectId: "realtimechatapplication-trilok",
+    storageBucket: "realtimechatapplication-trilok.appspot.com",
+    messagingSenderId: "965800671505",
+    appId: "1:965800671505:web:3f3238b4bf441f8708f4b0",
+    measurementId: "G-LCJX2K7LR6"
+});
+
+// Retrieve an instance of Firebase Messaging
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    
+    const notificationTitle = payload.notification.title || 'Background Message Title';
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/firebase-logo.png' // Change this to your notification icon path
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+});
